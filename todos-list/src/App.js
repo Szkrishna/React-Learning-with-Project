9e4d3cb789a56c8606edac65ba19c14ccbe63c31
Szkrishna <krisnas9792@gmail.com>
2024-12-21 12:54:48 +0530

    adding footer compont

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from "./MyComponents/Header";
+// import Footer from "./MyComponents/Footer";
+import { Footer } from './MyComponents/Footer';
 
 function App() {
   return (
     <>
       <Header/>
+      <Footer/>
     </>
   );
 }

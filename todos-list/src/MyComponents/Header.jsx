b3e6b4c9da62b6a611ae8050f9fee84553aacaab
Szkrishna <krisnas9792@gmail.com>
2024-12-24 13:30:38 +0530

    highlighting active router link so that it looks which link is active at that time

--- todos-list/src/MyComponents/Header.jsx
+++ todos-list/src/MyComponents/Header.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = (props) => {
+  const currentPage = useLocation().pathname;
   return (
-    <nav class="navbar navbar-expand-lg" style={{ backgroundColor: "rgba(52, 152, 219, 0.6)" }}>
-      <div class="container-fluid py-2 px-4">
-        {/* <Link class="navbar-brand" href="#">{props.title}</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "rgba(52, 152, 219, 0.6)" }}>
+      <div className="container-fluid py-2 px-4">
+        {/* <Link className="navbar-brand" href="#">{props.title}</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button> */}
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={currentPage === '/' ? 'nav-link active' : 'nav-link'} aria-current="page" to="/">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/about">About</Link>
+            <li className="nav-item">
+              <Link className={currentPage === '/about' ? 'nav-link active' : 'nav-link'} to="/about">About</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/contact">Contact</Link>
+            <li className="nav-item">
+              <Link className={currentPage === '/contact' ? 'nav-link active' : 'nav-link'} to="/contact">Contact</Link>
             </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/add-todo">Add Todo</Link>
+            <li className="nav-item">
+              <Link className={currentPage === '/add-todo' ? 'nav-link active' : 'nav-link'} to="/add-todo">Add Todo</Link>
             </li>
           </ul>
-          {props.search ? <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          {props.search ? <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-light" type="submit">Search</button>
           </form> : ""}
         </div>
